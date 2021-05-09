416b7ce30f2ca7abcbc5f389b6880146f5692ba5
Waidhoferj <jwaidhof@calpoly.edu>
2021-05-09 12:52:40 -0700

    added dropdown

--- src/pages/LocationViewer/LocationViewer.tsx
+++ src/pages/LocationViewer/LocationViewer.tsx
@@ -1,29 +1,33 @@
 import "./LocationViewer.scss";
 
 import React, { useState, useRef, useEffect } from "react";
-import { AnimatePresence } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 
 import NavBar from "../../components/NavBar/NavBar";
-import {useTourLocation, useSaveTour} from "../../hooks/tour-graph"
-import Api from "../../api"
+import { useTourLocation, useSaveTour, useTour } from "../../hooks/tour-graph";
+import Api from "../../api";
 
-import { Save } from "react-feather";
+import { Save, MoreHorizontal, X } from "react-feather";
 import Overlay from "./components/Overlay";
 import produce from "immer";
+import { useHistory } from "react-router";
+import { useParams } from "react-router-dom";
 
-type OverlayUpdate = (overlay : Overlay) => void;
+type OverlayUpdate = (overlay: Overlay) => void;
 
-const clamp = (val :number, min :number, max: number) => Math.min(Math.max(val, min), max);
+const clamp = (val: number, min: number, max: number) =>
+  Math.min(Math.max(val, min), max);
 
 // Would fetch this from the server
 
 export default function LocationViewer() {
   const [location, updateLocation] = useTourLocation();
-  const save = useSaveTour()
+  const save = useSaveTour();
   // const depthMap = useDepthMap(location.panorama);
   const locationRef = useRef<HTMLImageElement | null>(null);
+  const [showMenu, setShowMenu] = useState(false);
 
-  function addOverlay(e : React.MouseEvent) {
+  function addOverlay(e: React.MouseEvent) {
     const el = e.target as Element;
     const bounds = el.getBoundingClientRect();
     let x = e.clientX - bounds.left - 150;
@@ -31,12 +35,16 @@ export default function LocationViewer() {
     let y = e.clientY - bounds.top - 40;
     y = clamp(y, 0, bounds.height);
 
-    const overlay : Overlay = {title: "", description: "", position: [x,y,0], actions: []};
-    updateLocation(loc => {
-      loc.overlays.push(overlay)
-      return loc
-    })
-    
+    const overlay: Overlay = {
+      title: "",
+      description: "",
+      position: [x, y, 0],
+      actions: [],
+    };
+    updateLocation((loc) => {
+      loc.overlays.push(overlay);
+      return loc;
+    });
   }
 
   function getOverlayKey(overlayData: Overlay) {
@@ -44,24 +52,27 @@ export default function LocationViewer() {
   }
 
   function deleteOverlay(key: number) {
-    updateLocation(loc => {
-      loc.overlays.splice(key, 1)
-      return loc
-    })
+    updateLocation((loc) => {
+      loc.overlays.splice(key, 1);
+      return loc;
+    });
   }
 
-  function updateOverlay(key : number, update : OverlayUpdate) {
-    updateLocation(loc => {
+  function updateOverlay(key: number, update: OverlayUpdate) {
+    updateLocation((loc) => {
       loc.overlays[key] = produce(loc.overlays[key], update);
-      return loc
-    })
+      return loc;
+    });
   }
 
   return (
     <article className="LocationViewer">
       <NavBar className="absolute" title={location.title}>
-        <button className="wrapper">
+        <button className="nav-button wrapper">
           <Save onClick={save} />
+        </button>
+        <button className="nav-button wrapper">
+          <MoreHorizontal onClick={() => setShowMenu(true)} />
         </button>
       </NavBar>
       <div className="content">
@@ -72,7 +83,7 @@ export default function LocationViewer() {
           ref={locationRef}
         />
         <AnimatePresence>
-          {location.overlays.map((data,index) => {
+          {location.overlays.map((data, index) => {
             return (
               <Overlay
                 key={getOverlayKey(data)}
@@ -85,16 +96,60 @@ export default function LocationViewer() {
           })}
         </AnimatePresence>
       </div>
+      <Dropdown showMenu={showMenu} onClose={() => setShowMenu(false)} />
     </article>
   );
 }
 
 /**
- * 
+ *
  * @param imageUrl The Cloudinary resource URL from the tour graph.
  */
 function useDepthMap(imageUrl: string) {
   const [depthMap, setDepthMap] = useState<number[][]>([[]]);
-  useEffect(() => void Api.findDepth(imageUrl).then(setDepthMap))
-  return depthMap
+  useEffect(() => void Api.findDepth(imageUrl).then(setDepthMap));
+  return depthMap;
+}
+
+interface DropdownProps {
+  showMenu: boolean;
+  onClose: () => void;
+}
+
+function Dropdown({ showMenu, onClose }: DropdownProps) {
+  const { tourId, locationName } = useParams<{
+    tourId: string;
+    locationName: string;
+  }>();
+  const history = useHistory();
+  const updateTour = useTour()[1];
+
+  function deleteLocation() {
+    updateTour((tour) => {
+      delete tour.locations[locationName];
+      return tour;
+    });
+    history.push("/tour/" + tourId);
+  }
+  return (
+    <AnimatePresence>
+      {showMenu && (
+        <motion.div
+          className="ContextMenu"
+          initial={{ y: "-100%" }}
+          animate={{ y: 0 }}
+          exit={{ y: "-100%" }}
+        >
+          <header>
+            <button className="wrapper" onClick={onClose}>
+              <X size={20} />
+            </button>
+          </header>
+          <button className="wrapper" onClick={deleteLocation}>
+            Delete
+          </button>
+        </motion.div>
+      )}
+    </AnimatePresence>
+  );
 }
